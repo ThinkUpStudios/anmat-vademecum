588ac2a8e409e2f3c756dec45116e69c113b4e9f
mauroa <mag@xamarin.com>
2015-02-11 19:22:27 -0300

    Base iOS project structure added

---
+++ iOS/Anmat Vademecum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anmat Vademecum
+//
+//  Created by mag on 2/11/15.
+//  Copyright (c) 2015 Think Up Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
