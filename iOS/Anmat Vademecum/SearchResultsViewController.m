505510ff03d6e7234ff67b2fc3b9ef5e95df7a14
mauroa <mag@xamarin.com>
2015-02-20 10:05:44 -0300

    Main skeleton added
    - Added brand new empty app
    - Added skeleton Views
    - Added skeleton ViewControllers

---
+++ iOS/Anmat Vademecum/SearchResultsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SearchResultsViewController.m
+//  Anmat Vademecum
+//
+//  Created by mag on 2/20/15.
+//  Copyright (c) 2015 Think Up Studios. All rights reserved.
+//
+
+#import "SearchResultsViewController.h"
+
+@implementation SearchResultsViewController
+
+@end
---
+++ iOS/Anmat Vademecum/SearchViewController.m
@@ -0,0 +1,25 @@
+//
+//  SearchViewController.m
+//  Anmat Vademecum
+//
+//  Created by mag on 2/20/15.
+//  Copyright (c) 2015 Think Up Studios. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController()
+
+@property (weak, nonatomic) IBOutlet UIView *vwGenericName;
+@property (weak, nonatomic) IBOutlet UITextField *txtComercialName;
+@property (weak, nonatomic) IBOutlet UITextField *txtLaboratory;
+
+@end
+
+@implementation SearchViewController
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
