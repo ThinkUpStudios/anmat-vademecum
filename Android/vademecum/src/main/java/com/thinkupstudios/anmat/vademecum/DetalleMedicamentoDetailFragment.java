0a4b0ccfe2e500dd08e573ec8a23506f6e1ada6e
dariocamarro <dariocamarro@gmail.com>
2015-03-02 16:33:56 -0300

    se agrega iconos en los tabs.

--- Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/DetalleMedicamentoDetailFragment.java
+++ Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/DetalleMedicamentoDetailFragment.java
@@ -64,18 +64,23 @@ public class DetalleMedicamentoDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detallemedicamento_detail, container, false);
 
         this.setValores(rootView, this.medicamento, container);
+        Resources res = getResources();
 
         TabHost tabs=(TabHost)rootView.findViewById(android.R.id.tabhost);
         tabs.setup();
 
         TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
         spec.setContent(R.id.detalle);
-        spec.setIndicator("Detalle");
+        //spec.setIndicator("Detalle");
+        spec.setIndicator("", res.getDrawable(R.drawable.detalle));
         tabs.addTab(spec);
+        tabs.setHorizontalScrollBarEnabled(true);
 
-        spec=tabs.newTabSpec("mitab2");
+        spec=tabs.newTabSpec("");
         spec.setContent(R.id.formula);
-        spec.setIndicator("Formula");
+        //spec.setIndicator("Formula");
+        spec.setIndicator("", res.getDrawable(R.drawable.formula));
+
         tabs.addTab(spec);
 
         tabs.setCurrentTab(0);
