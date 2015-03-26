c3aefcee6e2d24c5c328caa201c3fd9c0d1119bd
mauroa <mag@xamarin.com>
2015-03-26 18:18:39 -0300

    Added Contact Info popup page on Menu screen

--- iOS/Anmat Vademecum/MenuViewController.m
+++ iOS/Anmat Vademecum/MenuViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UISelectableButton *btnMeds;
 @property (weak, nonatomic) IBOutlet UISelectableButton *btnComponents;
 @property (weak, nonatomic) IBOutlet UISelectableButton *btnInfo;
+- (IBAction)btnContact:(id)sender;
 
 @end
 
@@ -30,6 +31,35 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (IBAction)btnContact:(id)sender {
+    UIAlertController *contactSheet = [UIAlertController alertControllerWithTitle:@"ANMAT Responde" message:@"Datos de contacto" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    NSString *phoneNumber = @"0800-333-1234";
+    
+    UIAlertAction *phoneAction = [UIAlertAction actionWithTitle:[@"Tel: " stringByAppendingString:phoneNumber] style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"telprompt://" stringByAppendingString:phoneNumber]]];
+        [contactSheet dismissViewControllerAnimated:YES completion:nil];
+    }];
+    UIAlertAction *faxAction = [UIAlertAction actionWithTitle:@"Fax: (011) 4340-0800/5252-8200 int 1159" style:UIAlertActionStyleDefault handler:nil];
+    
+    NSString *mailAddress = @"responde@anmat.gov.ar";
+    
+    UIAlertAction *mailAction = [UIAlertAction actionWithTitle:[@"Email: " stringByAppendingString:mailAddress] style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"mailto://" stringByAppendingString:mailAddress]]];
+        [contactSheet dismissViewControllerAnimated:YES completion:nil];
+    }];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancelar" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        [contactSheet dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    [contactSheet addAction:phoneAction];
+    [contactSheet addAction:faxAction];
+    [contactSheet addAction:mailAction];
+    [contactSheet addAction:cancelAction];
+    [self
+     presentViewController:contactSheet animated:YES completion:nil];
 }
 
 -(void) setBorder: (UISelectableButton *) button {
