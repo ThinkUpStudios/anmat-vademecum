c0f06c5796e1488a55996d63681cce1a4c38085e
Facundo Costa Zini <fcostazini@conexia.com>
2015-10-19 17:36:55 -0300

    cambio de entidad para respetar la nueva tabla

--- Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/providers/PrincipioActivoProvider.java
+++ Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/providers/PrincipioActivoProvider.java
@@ -54,7 +54,7 @@ public class PrincipioActivoProvider extends GenericProvider {
 
             while (!cursor.isAfterLast()) {
                 principioActivo = cursorToPrincipioActivo(cursor);
-                resultado.add(principioActivo.getNombre());
+                resultado.add(principioActivo.getIfa());
                 if (principioActivo.getOtrosNombres() != null && !principioActivo.getOtrosNombres().isEmpty()) {
                     String[] otrosNombre = principioActivo.getOtrosNombres().split("\\?");
                     for (String otroNombre : otrosNombre) {
@@ -76,15 +76,18 @@ public class PrincipioActivoProvider extends GenericProvider {
 
     private PrincipioActivo cursorToPrincipioActivo(Cursor cursor) {
         PrincipioActivo principioActivo = new PrincipioActivo();
-        principioActivo.setNombre(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[0])));
-        principioActivo.setAccionTerapeutica(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[1])));
-        principioActivo.setIndicaciones(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[2])));
-        principioActivo.setPresentacion(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[3])));
-        principioActivo.setPosologia(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[4])));
-        principioActivo.setDuracion(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[5])));
+        principioActivo.setIfa(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[0])));
+        principioActivo.setClasificacionTerapeutica(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[1])));
+        principioActivo.setMecanismoAccion(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[2])));
+        principioActivo.setFarmacocinetica(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[3])));
+        principioActivo.setIndicaciones(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[4])));
+        principioActivo.setPosologia(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[5])));
         principioActivo.setContraindicaciones(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[6])));
-        principioActivo.setObservaciones(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[7])));
-        principioActivo.setOtrosNombres(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[8])));
+        principioActivo.setInteracciones(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[7])));
+        principioActivo.setReaccionesAdversas(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[8])));
+        principioActivo.setInformacionAdicional(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[9])));
+        principioActivo.setBibliografia(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[10])));
+        principioActivo.setOtrosNombres(cursor.getString(cursor.getColumnIndex(PrincipiosActivosTable.COLUMNS[11])));
 
         return principioActivo;
     }
--- Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/providers/tables/PrincipiosActivosTable.java
+++ Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/providers/tables/PrincipiosActivosTable.java
@@ -11,15 +11,22 @@ public class PrincipiosActivosTable {
     public static String[] COLUMN_NAME = {"principio"};
 
     public static String TABLE_NAME = "principios_activos";
-    public static String[] COLUMNS =   {"principio",
-                                        "accion",
-                                        "indicaciones",
-                                        "presentacion",
-                                        "posologia",
-                                        "duracion",
-                                        "contraindicaciones",
-                                        "observaciones",
-                                        "otros_nombres"};
+    public static String[] COLUMNS =       {"ifa",
+                                            "clasificacionTerapeutica",
+                                            "descripcionFarmacologia",
+                                            "mecanismoAccion",
+                                            "farmacocinetica",
+                                            "indicaciones",
+                                            "posologia",
+                                            "contraindicaciones",
+                                            "interacciones",
+                                            "reaccionesAdversas",
+                                            "informacionAdicional",
+                                            "bibliografia",
+                                            "otrosNombres"};
 
+
+
+    
 
 }
