2e58f1375a48b5d1e204c3060cd1a3d097d584b3
Facundo Costa Zini <fcostazini@conexia.com>
2015-11-13 12:25:06 -0300

    fix esteticos

--- Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/DetalleMedicamentoEmbarazoActivity.java
+++ Android/vademecum/src/main/java/com/thinkupstudios/anmat/vademecum/DetalleMedicamentoEmbarazoActivity.java
@@ -34,10 +34,25 @@ public class DetalleMedicamentoEmbarazoActivity extends Activity {
 
         setContentView(R.layout.activity_detalle_medicamento_embarazo);
         TextView txtCabecera = (TextView) findViewById(R.id.txt_medicamento_embarazo);
-        txtCabecera.setText(Html.fromHtml(medicamentoEmbarazoBO.getCabecera()));
+        txtCabecera.setText(Html.fromHtml("<p><b>La FDA según el riesgo clasifica los fármacos en las siguientes categorías:</b></p>" +
+                "<p><b>Categoría A:</b></p>" +
+                "<p>Estudios adecuados y bien controlados no han demostrado riesgo para el feto en el primer trimestre de embarazo y no existen evidencias de riesgo en trimestres posteriores. La posibilidad de daño fetal parece remota.</p>" +
+                "<p><b>Categoría B:</b></p>" +
+                "<p>Indica una de las siguientes posibilidades:</p>" +
+                "<p> • Estudios en animales no indican riesgo teratogénico fetal, pero esto no ha sido confirmado en embarazadas</p>" +
+                "<p> • Estudios en animales muestran cierto potencial teratógeno, pero estudios bien controlados con gestantes no han demostrado riesgos para el feto en el primer trimestre y no existen evidencias para el feto en trimestres posteriores</p>" +
+                "<p><b>Categoría C:</b></p>" +
+                "<p>Se suele asignar a fármacos cuya administración solo debe considerarse si el beneficio esperado justifica el potencial riesgo para el feto.</p>" +
+                "<p>Indica una de las sig. posibilidades:</p>" +
+                "<p> • Estudios sobre animales han detectado efecto teratógeno o embriocida del fármaco, pero aún no se ha ensayado en la mujer.</p>" +
+                "<p> • No se dispone de estudios ni en animales ni en mujeres.</p>" +
+                "<p><b>Categoría D:</b></p>" +
+                "<p>Se dispone de evidencia de efectos teratógenos sobre el feto humano y por tanto de la existencia de un claro riesgo. Sin embargo el beneficio obtenido con estos medicamentos puede superar el riesgo esperado y hacer aconsejable su uso(situaciones límites de posible muerte materna, afecciones graves en las que no es posible usar alternativas más seguras o éstas son ineficaces…)</p>  " +
+                "<p><b>Categoría X:</b></p>" +
+                "<p>Estudios en animales o en  humanos han demostrado anomalías congénitas manifiestas, existen evidencias de riesgo fetal basadas enla experiencia en embarazadas y los riesgos superan claramente cualquier posible beneficio a obtener, por lo que los fármacos están absolutamente contraindicados.</p>"));
 
 
-        for (GrupoPrincipiosEmbarazo grupo:this.medicamentoEmbarazoBO.getGrupos()) {
+        for (GrupoPrincipiosEmbarazo grupo : this.medicamentoEmbarazoBO.getGrupos()) {
             this.armarGrupo(grupo);
         }
 
@@ -55,8 +70,7 @@ public class DetalleMedicamentoEmbarazoActivity extends Activity {
         nombreGrupo.setText(grupo.getNombreGrupo());
         contenedor.addView(laViewInflada);
         laViewInflada.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Bundle b = new Bundle();
                 /*
                 PairHeadDetail[] toRender = new PairHeadDetail[1];
@@ -86,30 +100,29 @@ public class DetalleMedicamentoEmbarazoActivity extends Activity {
     }
 
 
-
     @Override
     protected void onResume() {
         super.onResume();
         ((MiAplicacion) this.getApplication()).updateCache(new DatabaseHelper(this), false);
     }
 
-private class CategoryClickHandler implements View.OnClickListener {
+    private class CategoryClickHandler implements View.OnClickListener {
 
-    private PairHeadDetail[] toRender;
+        private PairHeadDetail[] toRender;
 
-    public CategoryClickHandler(PairHeadDetail[] toRender) {
-        this.toRender = toRender;
+        public CategoryClickHandler(PairHeadDetail[] toRender) {
+            this.toRender = toRender;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Bundle b = new Bundle();
+            b.putSerializable("toRender", new ToRenderWrapper(this.toRender));
+            Intent i = new Intent(DetalleMedicamentoEmbarazoActivity.this, DetalleCategoriaActivity.class);
+            i.putExtras(b);
+            DetalleMedicamentoEmbarazoActivity.this.startActivity(i);
+
+        }
     }
-
-    @Override
-    public void onClick(View view) {
-        Bundle b = new Bundle();
-        b.putSerializable("toRender", new ToRenderWrapper(this.toRender));
-        Intent i = new Intent(DetalleMedicamentoEmbarazoActivity.this, DetalleCategoriaActivity.class);
-        i.putExtras(b);
-        DetalleMedicamentoEmbarazoActivity.this.startActivity(i);
-
-    }
-}
 
 }
